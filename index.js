55e916e1b3a05e155d6203b45540870866b583f0
tanim-k <tanimkhan877@gmail.com>
2022-04-22 13:12:01 +0600

    nodemon installation done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000
 
 app.get('/', (req, res) => {
-    res.send('oma eta ami kita korilailam vala lager ba')
+    res.send('oma eta ami kita korilailam vala lager ba. ki ekta obstha.. kicchu bujhtesi na.. but node moja lagtese. change koreo dekhi ki hoy     valani ba:. kita re ba.')
 })
 
 app.listen(port, () => {
