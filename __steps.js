ec773371e5480509092f3fcca403fdd74b3755fc
tanim-k <tanimkhan877@gmail.com>
2022-04-23 13:43:45 +0600

    server site is ready

---
+++ __steps.js
@@ -0,0 +1,35 @@
+/* **
+----------------------*
+basic server setup
+-----------------------*
+* 1. create a folder (manually or mkdir)
+2. open command line to that folder
+3. nmp init
+4. npm install express
+
+-------------------------
+for express/ node server: 
+-------------------
+1. require express.
+2. create app variable
+3. declare port
+4. set app.get('/')
+5. listen to port
+6. node index.js
+7. check your browser for that port*
+-----------------------------
+get er jonne ki kora lagbe
+1. cors middleware
+2. app.get e simple object or simple array pathaite parbo
+-----------------------------
+post:
+1. post korar kichu rules ache
+2. react router thinking development
+3. post pacchi req.body er moddhe
+4. use middleware or body parser
+5. set id then push to users temporary
+6. user ke ui te pathai disi
+---------------------------
+app.listen: imp step
+1.
+*/
